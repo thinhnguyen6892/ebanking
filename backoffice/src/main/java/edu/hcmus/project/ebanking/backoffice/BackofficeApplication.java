f13af64cc880f27cf16ee7ca3b87d836bf3e5948
thinhnguyen <thinhnguyen6892@gmail.com>
2020-02-08 16:31:13 +0700

    [Func-01] Init maven project with Spring Boot

---
+++ backoffice/src/main/java/edu/hcmus/project/ebanking/backoffice/BackofficeApplication.java
@@ -0,0 +1,26 @@
+package edu.hcmus.project.ebanking.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class BackofficeApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackofficeApplication.class, args);
+	}
+
+}
---
+++ backoffice/src/test/java/edu/hcmus/project/ebanking/backoffice/BackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hcmus.project.ebanking.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
