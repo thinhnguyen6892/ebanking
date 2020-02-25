753dda25ffeb3645091e21384ce465cbcdb66be8
minhtrong <trong20098@gmail.com>
2020-02-25 17:34:32 +0700

    merge Trong branch with master

--- backoffice/src/main/java/edu/hcmus/project/ebanking/backoffice/resource/debt/DebtResourceRestController.java
+++ backoffice/src/main/java/edu/hcmus/project/ebanking/backoffice/resource/debt/DebtResourceRestController.java
@@ -3,11 +3,13 @@ package edu.hcmus.project.ebanking.backoffice.resource.debt;
 import edu.hcmus.project.ebanking.backoffice.model.Debt;
 import edu.hcmus.project.ebanking.backoffice.repository.DebtRepository;
 import edu.hcmus.project.ebanking.backoffice.resource.exception.ResourceNotFoundException;
+import edu.hcmus.project.ebanking.backoffice.service.DebtService;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,68 +17,38 @@ import java.util.Optional;
 public class DebtResourceRestController {
 
     @Autowired
-    private DebtRepository DebtRepository;
+    private DebtService debtService;
 
     @GetMapping("/debt")
-    public List<Debt> getAllDebt() {
-        return DebtRepository.findAll();
+    public List<DebtDto> getAllDebt() {
+        return debtService.GetAllDebt();
     }
 
     @GetMapping("/debt/{id}")
-    public DebtDto findDebt(@PathVariable int id){
-        Optional<Debt> DebtOp = DebtRepository.findById(id);
-        if(DebtOp.isPresent()){
-            Debt debt = DebtOp.get();
-            DebtDto dto = new DebtDto();
-            dto.setId(debt.getId());
-            dto.setType(debt.getType());
-            dto.setCreateDate(debt.getCreateDate());
-            dto.setEndDate(debt.getEndDate());
-            dto.setStatus(debt.getStatus());
-            return dto;
-        }
-        throw new ResourceNotFoundException("Debt not found");
+    public DebtDto findDebt(@Valid @PathVariable int id){
+        return debtService.findDebt(id);
+    }
+
+    @GetMapping("/debt/{holder}")
+    public List<DebtDto> findDebtByHolder(@Valid @PathVariable int holder){
+        return debtService.findDebtbyHolder(holder);
     }
 
     @PostMapping("/debt/create")
-    public ResponseEntity<DebtDto> createBank(@RequestBody DebtDto dto) {
-        Optional<Debt> Debtop = DebtRepository.findById(dto.getId());
-        if(!Debtop.isPresent()) {
-            Debt newDebt = new Debt();
-            newDebt.setId(dto.getId());
-            newDebt.setType(dto.getType());
-            newDebt.setCreateDate(dto.getCreateDate());
-            newDebt.setEndDate(dto.getEndDate());
-            newDebt.setStatus(dto.getStatus());
-            newDebt = DebtRepository.save(newDebt);
-            return new ResponseEntity<DebtDto>(dto, HttpStatus.OK);
-        }
-        throw new ResourceNotFoundException("Debt have existed");
+    public ResponseEntity<DebtDto> createDebt(@RequestBody DebtDto dto) {
+        boolean result = debtService.createDebt(dto);
+        return new ResponseEntity<DebtDto>(dto, HttpStatus.OK);
     }
 
     @PutMapping("/debt/update/{id}")
-    public ResponseEntity<DebtDto> updateBank(@RequestBody DebtDto dto, @PathVariable int id){
-        Optional<Debt> BankOp = DebtRepository.findById(id);
-        if(BankOp.isPresent()) {
-            Debt upDebt = BankOp.get();
-            upDebt.setType(dto.getType());
-            upDebt.setCreateDate(dto.getCreateDate());
-            upDebt.setEndDate(dto.getEndDate());
-            upDebt.setStatus(dto.getStatus());
-            upDebt = DebtRepository.save(upDebt);
-            return new ResponseEntity<DebtDto>(dto, HttpStatus.OK);
-        }
-        throw new ResourceNotFoundException("Debt not found");
+    public ResponseEntity<DebtDto> updateDebt(@RequestBody DebtDto dto, @PathVariable int id){
+        boolean result = debtService.updateDebt(dto, id);
+        return new ResponseEntity<DebtDto>(dto, HttpStatus.OK);
     }
 
     @DeleteMapping("/debt/delete/{id}")
-    public ResponseEntity<Void> delete(@PathVariable int id) {
-        Optional<Debt> deBank = DebtRepository.findById(id);
-        if(deBank.isPresent())
-        {
-            DebtRepository.deleteById(id);
-            return ResponseEntity.noContent().build();
-        }
-        throw new ResourceNotFoundException("Debt not found");
+    public ResponseEntity<Void> deleteDebt(@PathVariable int id) {
+        boolean result = debtService.deleteDebt(id);
+        return ResponseEntity.noContent().build();
     }
 }
